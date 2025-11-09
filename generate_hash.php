c6bb9ff2c51a5a3dbcf40654bb62092458a6562e
Fernando Lopes <50033985+finandolopes@users.noreply.github.com>
2025-11-09 14:47:14 -0300

    correções  finais

--- generate_hash.php
+++
@@ -1,8 +0,0 @@
-<?php
-$senha = '0m3g4r3d';
-$hash = password_hash($senha, PASSWORD_DEFAULT);
-
-echo "Senha: $senha\n";
-echo "Hash gerado: $hash\n";
-echo "Verificação: " . (password_verify($senha, $hash) ? 'OK' : 'ERRO') . "\n";
-?>
